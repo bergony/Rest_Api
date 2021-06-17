022d7886deef096eb48e6671544bfc4ceab887f9
Bergony <Bergony@hotmail.com>
2021-06-17 19:44:57 -0300

    Initial Commit.

---
+++ src/test/java/bergony/ml/rest_api/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package bergony.ml.rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
